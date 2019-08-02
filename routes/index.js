8f2ddcd319767b2934920b70588f09a3ccbfb278
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-02 05:05:56 +0300

    documented

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 const { Router } = require("express");
-const URL = require("../models/url");
+const URL = require("../models/url"); // the model
 
-const router = Router();
+const router = Router(); // initialize express router
 
 // @route   /:code
 // @desc    redirect to the longUrl
 router.get("/:code", async (req, res) => {
-  const code = req.params.code;
-  const url = await URL.findOne({ urlCode: code });
+  const code = req.params.code; // get the code
+  const url = await URL.findOne({ urlCode: code }); // find the code
 
+  // if code not exists
   if (!url) {
-    return res.status(404).send({ message: "url not found" });
+    return res.status(404).send({ message: "url not found" }); // return 404 NOT Found response
   }
 
-  res.redirect(301, url.longUrl);
+  res.redirect(301, url.longUrl); // if the url is exists then redirect
 });
 
 module.exports = router;
