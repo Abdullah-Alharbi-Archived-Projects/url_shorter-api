2e560765f43dc43781fa6e644b95ebdfc344e83e
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-02 05:09:06 +0300

    disable version key

--- models/url.js
+++ models/url.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const urlSchema = new mongoose.Schema({
-  urlCode: String,
-  longUrl: String,
-  shortUrl: String,
-  date: { type: String, default: Date.now }
-});
+const urlSchema = new mongoose.Schema(
+  {
+    urlCode: String,
+    longUrl: String,
+    shortUrl: String,
+    date: { type: String, default: Date.now }
+  },
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("Url", urlSchema);
